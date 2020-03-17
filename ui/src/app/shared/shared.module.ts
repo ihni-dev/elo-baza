4d1c84325c066df56801c09c03e84ed73adaed50
Marek Frankowicz <frankowicz.marek@gmail.com>
2020-03-17 20:42:07 +0000

    Add material module

--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
