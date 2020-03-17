422a8f0d12a7a37bd5a2e92dca1881457b1a221b
Marek Frankowicz <frankowicz.marek@gmail.com>
2020-03-17 20:39:11 +0000

    Add basic modules structure

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
+    FeaturesModule,
+    SharedModule,
+
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
