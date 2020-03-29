92985a9ecf1ebb330e72ffd1d96e4985b7b1fa01
Marek Frankowicz <frankowicz.marek@gmail.com>
2020-03-29 13:37:40 +0000

    Implement footer

---
+++ ui/src/app/core/layout/footer/footer.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer.component';
+
+@NgModule({
+  exports: [FooterComponent],
+  declarations: [FooterComponent],
+})
+export class FooterModule {}
