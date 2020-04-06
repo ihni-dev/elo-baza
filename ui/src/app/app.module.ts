be775d2399ced44a621243d2bfd70bbd12f5cd26
Marek Frankowicz <frankowicz.marek@gmail.com>
2020-04-06 18:18:42 +0200

    Add ngxs support

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,9 +5,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarModule } from './core/layout/navbar/navbar.module';
 
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
+
     BrowserModule,
     BrowserAnimationsModule,
 
