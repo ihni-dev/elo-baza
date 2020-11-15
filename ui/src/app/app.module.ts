c20b94f158251c401a4a8730b150e92fe2579011
Marek Frankowicz <frankowicz.marek@gmail.com>
2020-11-15 17:44:45 +0100

    Clean package json

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,18 +5,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarModule } from './core/layout/navbar/navbar.module';
 
-import { NgxsModule } from '@ngxs/store';
-import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
 import { PageTitleModule } from './core/layout/page-title/page-title.module';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    NgxsModule.forRoot(),
-    NgxsRouterPluginModule.forRoot(),
-    environment.plugins,
-
     BrowserModule,
     BrowserAnimationsModule,
 
--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -2,16 +2,9 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/',
-  plugins: [
-    NgxsLoggerPluginModule.forRoot(),
-    NgxsReduxDevtoolsPluginModule.forRoot()
-  ]
+  apiUrl: 'http://localhost:5000/api/'
 };
 
 /*
